0fe5838398576dd821b02ba77543c6c3ab28acb0
Fahad <sfahadahmed@gmail.com>
2018-12-29 17:50:04 +0400

    Adding UI enhancements using Bootstrap

--- src/components/TodoItemsList.vue
+++ src/components/TodoItemsList.vue
@@ -1,26 +1,32 @@
 <template>
-    <div>
-        <div>
-            <form v-on:submit.prevent="add">
-                <input type="text" name="todo" id="todo" v-model="todoText" placeholder="e.g. meeting at 10:00 AM">&nbsp;
-                <button>Add</button>
-            </form>
+    <div id="myroot" class="container">
+        <div class="card">
+            <div class="card-body">
+                <form class="form-inline" v-on:submit.prevent="add">
+                <div class="form-group">
+                <label for="todo">Todo: </label>
+                <input type="text" class="form-control" name="todo" id="todo" v-model="todoText" placeholder="e.g. meeting at 10:00 AM">&nbsp;
+                <button class="btn btn-primary">Add</button>
+                </div>
+                </form>
+            </div>
         </div>
-        
-        <div>
         <hr size="1">
-        <ul style="text-align: left">
-            <li
-                v-for="(todoItem, index) in todoItems"
-                v-bind:key="todoItem.id"
-                v-bind:title="todoItem.title">
-                {{todoItem.title}}
-                &nbsp;
-                <button v-on:click="remove(index)">Remove</button>
-            </li>
-        </ul>
-    </div>
-
+        <div class="card">
+            <div class="card-body">
+                <ul class="list-group" style="text-align: left">
+                    <li class="list-group-item" 
+                        v-for="(todoItem, index) in todoItems"
+                        v-bind:key="todoItem.id"
+                        v-bind:title="todoItem.title">
+                        {{todoItem.title}}
+                        &nbsp;
+                        <button class="btn btn-warning" v-on:click="remove(index)">Remove</button>
+                    </li>
+                </ul>
+                <span v-if="this.todoItems.length == 0">Add some todo items...</span>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -50,5 +56,7 @@ export default {
 </script>
 
 <style>
-
+.center {
+    text-align: center;
+}
 </style>
